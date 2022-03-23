b49e99964131e9dd14172290a203a5fc9f564a63
Lidbey <wickiwojtek@gmail.com>
2022-03-23 20:33:36 +0100

    HeroDodany

--- GameState/Hero.py
+++ GameState/Hero.py
@@ -1,4 +1,21 @@
 class Hero:
     def __init__(self):
+        self.heroType = 0
         self.health = 0
         self.attack = 0
+        self.sleeping = 0
+        self.frozen = 0
+        self.rush = 0
+        self.taunt = 0
+        self.divineShield = 0
+        self.isBuffed = 0
+        self.stealth = 0
+        self.windFury = 0
+        self.hasWeapon = 0
+        self.isImmune = 0
+        self.armour = 0
+        self.weaponId = 0
+        self.weaponPoisonous = 0
+        self.weaponAtt = 0
+        self.weaponDurability = 0
+        self.weaponBuff = 0
