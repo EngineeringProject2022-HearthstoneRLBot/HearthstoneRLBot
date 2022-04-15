b1c0cb5516c0fe365ca6c7dab47752fe6fca84cf
Lidbey <wickiwojtek@gmail.com>
2022-04-15 18:31:23 +0200

    Hero początkowe mapowanie

--- GameState/Hero.py
+++ GameState/Hero.py
@@ -1,21 +1,40 @@
 class Hero:
-    def __init__(self):
-        self.heroType = 0
-        self.health = 0
-        self.attack = 0
-        self.sleeping = 0
-        self.frozen = 0
-        self.rush = 0
-        self.taunt = 0
-        self.divineShield = 0
-        self.isBuffed = 0
-        self.stealth = 0
-        self.windFury = 0
-        self.hasWeapon = 0
-        self.isImmune = 0
-        self.armour = 0
-        self.weaponId = 0
-        self.weaponPoisonous = 0
-        self.weaponAtt = 0
-        self.weaponDurability = 0
-        self.weaponBuff = 0
+    def __init__(self, character):
+        self.player = character
+
+
+    def mapToInput(self):
+        hero = [0]*36
+        weapon = [0]*3
+        weapon[0] = self.player.weapon.atk
+        weapon[1] = self.player.weapon.durability
+        weapon[2] = self.player.weapon.cost
+        hero[0] = 0
+        hero[1] = self.player.characters[0].health
+        hero[2] = self.player.characters[0].atk
+        hero[3] = 30
+        hero[4] = 0
+        hero[5] = self.player.characters[0].exhausted
+        hero[6] = 1 if self.player.characters[0].frozen else 0
+        hero[7] = 0
+        hero[8] = 0
+        hero[9] = 1 if self.player.characters[0].taunt else 0
+        hero[10] = 0
+        hero[11:18] = 0
+        hero[19] = len(self.player.characters[0].buffs)
+        hero[20:22] = 0
+        hero[23]
+        hero[24]
+        hero[25]
+        hero[26]
+        hero[27]
+        hero[28] = 1 if self.player.characters[0].health < 30 else 0
+        hero[29] = 1 if self.player.characters[0].windfury > 0 else 0
+        hero[30] = 1 if self.player.characters[0].lifesteal else 0
+        hero[31] = 1 if self.player.weapon is None else 0
+        hero[32] = 1 if self.player.characters[0].immune else 0
+        hero[33] = 1 if self.player.characters[0].cant_attack else 0
+        hero[34] = self.player.characters[0].armor
+        hero[35] = int(self.player.card_id[5:7])
+        hero[36] = 1 if self.player.characters[0].poisonous else 0
+        hero[37] = len(self.player.secrets)
