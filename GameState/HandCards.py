3eea8a88d255c50097df11090b35a213dd856467
w_wicki <w.wicki@axis.pl>
2022-06-01 19:05:02 +0200

    Dodanie wyciągania: Randomness (&1<<31) Left (&1<<28) Right(&1<<27) Conditional(&1<<26) Self(&1<<25) Enemy(&1<<24) Ally(&1<<23) Minion(&1<<22) Hero(&1<<21) Ile razy (%16)

--- GameState/HandCards.py
+++ GameState/HandCards.py
@@ -277,12 +277,111 @@ def decodeTarget(target):
         return decodeBoardPosition(target.direction)+"("+decodeTarget(target.child)+")"
     return " Unknown "
 
+
+def decodeFuncSelector2(func):
+    if func == fireplace.dsl.selector.SELF:
+        return 1 << 25
+    if func == fireplace.dsl.selector.TARGET:
+        return ~0
+    if func == fireplace.dsl.selector.OTHER_CLASS_CHARACTER:
+        return ~0
+    if func == fireplace.dsl.selector.FRIENDLY_CLASS_CHARACTER:
+        return ~0
+    if func == fireplace.dsl.selector.LEFTMOST_HAND:
+        return ~0
+    if func == fireplace.dsl.selector.RIGHTMOST_HAND:
+        return ~0
+    if func == fireplace.dsl.selector.OWNER:
+        return ~0
+    if func == fireplace.dsl.selector.ATTACK_TARGET:
+        return ~0
+
+def decodeOp2(left, op, right):
+    if op == operator.or_:
+        return left | right
+    if op == operator.add:
+        return "?+?" #tu chce wyjatek bo tego chyba nie bedzie
+    if op == operator.and_:
+        return left & right
+    if op == operator.sub:
+        return left & (~right)
+    if op == operator.eq:
+        return left | right # nie wiem w sumie
+    if op == operator.le:
+        return left | right # nie wiem w sumie
+    return ">O<"
+
+def decodeEnum2(enum):
+    if isinstance(enum, str):
+        return 0
+    if enum == fireplace.dsl.selector.Zone.PLAY:
+        return ~0
+    if enum == fireplace.dsl.selector.CardType.MINION:
+        return 1 << 22 | 1 << 23 | 1 << 24 | 1 << 25 | 1 << 26# czy na self mogę zagra
+    if enum == fireplace.dsl.selector.CardType.HERO: #na hero sie gra np leczenie/buffa
+        return 1 << 21 | 1 << 23 | 1 << 24 | 1 << 26
+    if enum == fireplace.dsl.selector.CardType.PLAYER: #na gracza sie gra np przyzwanie(przyzwij minionka)
+        return (1<<23) | (1<<24) #aktywuję self i enemy aby pozniej kontroler mogl cos wylaczyc
+    if enum == fireplace.dsl.selector.GameTag.CONTROLLER:
+        return 0
+    if enum == fireplace.dsl.selector.GameTag.DORMANT:
+        return 0
+    if enum == fireplace.dsl.selector.GameTag.DAMAGE:
+        return ~(1 << 26)
+    return ~(1 << 26)
+
+def decodeController2(controller):
+    if isinstance(controller, fireplace.dsl.selector.Opponent):
+        return ~(1 << 23) & ~(1 << 25)
+    else:
+        return ~(1 << 24)
+
+def decodeBoardPosition2(direction):
+    if direction == fireplace.dsl.selector.BoardPositionSelector.Direction.LEFT:
+        return 1 << 28
+    elif direction == fireplace.dsl.selector.BoardPositionSelector.Direction.RIGHT:
+        return 1 << 27
+    else:
+        return " >D<"
+
+# 1 << 31 oznacza random
+# ostatnie 4 bity(czyli max. 16) oznaczają 'times' czyli ile celów losuje
+# jeśli target.times będzie >=16 to mam nadzieję że tak nie będzie bo się spowrotem wyzeruje
+def decodeTarget2(target):
+    if target is None:
+        return 0 #unknown value(co to znaczy w ogóle ze target is none)
+    if isinstance(target, fireplace.dsl.selector.RandomSelector):
+        return (1 << 31)+decodeTarget2(target.child)+(target.times%16)
+
+    if isinstance(target, fireplace.dsl.selector.SetOpSelector):
+        return decodeOp2(decodeTarget2(target.left), target.op, decodeTarget2(target.right))
+
+    if isinstance(target, fireplace.dsl.selector.EnumSelector):
+        return decodeEnum2(target.tag_enum)
+
+    if isinstance(target, fireplace.dsl.selector.ComparisonSelector):
+        return decodeOp2(decodeTarget2(target.left), target.op, decodeTarget2(target.right))
+
+    if isinstance(target, fireplace.dsl.selector.AttrValue):
+        return decodeEnum2(target.tag)
+    if isinstance(target, fireplace.dsl.selector.Controller):
+        return decodeController2(target)
+    if isinstance(target, fireplace.dsl.selector.FuncSelector):
+        return decodeFuncSelector2(target)
+    if isinstance(target, int):
+        return 0
+    if isinstance(target, fireplace.dsl.selector.BoardPositionSelector):
+        return decodeBoardPosition2(target.direction)
+    return " Unknown " #wyjatek i dobrze
+
 def mapEndOfTurn(card):
     endOfTurnEffect = {}
     for x in card.data.scripts.events:
         if isinstance(x, fireplace.actions.EventListener) and isinstance(x.trigger, fireplace.actions.EndTurn):
             print("znalazlem end turn efekt u "+card.data.strings[GameTag.CARDNAME]['enUS'])
             for y in x.actions:
+                print("0bR00LRCSEAMH00000000000000000TIME")
+                print(bin(decodeTarget2(y._args[0])).zfill(34))
                 print(decodeTarget(y._args[0]))
                 if isinstance(y, fireplace.actions.Hit):
                     print("Znalazlem damage "+str(y._args[1]))
