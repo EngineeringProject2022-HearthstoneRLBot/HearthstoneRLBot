0a741b560d433ab3ed1b6771b6b04c538fff8c9e
Kacper <kacperkrzeminski123@gmail.com>
2022-08-14 14:10:54 +0200

    small corrections

--- GamePlayer.py
+++ GamePlayer.py
@@ -10,9 +10,10 @@ from GameCommunication import checkValidActionsSparse, playTurnSparse
 from GameSetupUtils import mulliganRandomChoice
 from GameState import InputBuilder
 from montecarlo.montecarlo import MonteCarlo
-from montecarlo.node import Node
+from montecarlo.node import Node, NoChildException
 
 RANDOM_MOVE_SAMPLES = 5
+
 
 def _setup_game(data):
     # we setup our game here
@@ -32,7 +33,8 @@ def _setup_game(data):
     mulliganRandomChoice(game)
     return game
 
-def playGame(model, simulations, seedObject = None):
+
+def playGame(model, simulations, seedObject=None):
     if seedObject != None:
         random.setstate(seedObject)
     data = []
@@ -50,39 +52,44 @@ def playGame(model, simulations, seedObject = None):
 
     try:
         while True:
-            # states should be sync'd, doesnt matter whose tree we use here
-            currPlayer = montecarlo[0].root_node.game.current_player.entity_id - 1
+            currPlayer = game.current_player.entity_id - 1
 
-            (currTree, otherTree) = (montecarlo[0], montecarlo[1]) if currPlayer == 1 else (montecarlo[1], montecarlo[0])
+            (currTree, otherTree) = (montecarlo[0], montecarlo[1]) if currPlayer == 1 else (
+                montecarlo[1], montecarlo[0])
             currInput = InputBuilder.convToInput(currTree.root_node.game, currPlayer)
-            currTree.simulate(simulations)  # number of simulations per turn. do not put less than 2
+            try:
+                currTree.simulate(simulations)  # number of simulations per turn. do not put less than 2
+            except NoChildException as e:
+                # handle this issue, record it into the data
+                raise GameOver
+
             probabilities = currTree.get_probabilities()
 
-            currTree.root_node = currTree.make_exploratory_choice()
+            action = currTree.make_exploratory_choice()
 
             # else:
             #    montecarlo.root_node = montecarlo.make_choice(currPlayer)
             #    #rest of moves are the network playing "optimally"
 
-            if currTree.root_node.visits != 0:
-                currTree.root_node.visits -= 1
-
             # zamieniłem bo chcemy appendować co zrobiliśmy i dopiero wtedy zagrać turę - ten ostatni ruch
             # spowodował naszą wygraną (co nie byłoby zapisane do pliku, bo exception)
-            print("Turn: " + str(currTree.root_node.parent.game.turn) + ", Action:" + str(currTree.root_node.state))
+            print("Turn: " + str(game.turn) + ", Action:" + str(action))
 
             data.append(((currInput[:, :, :, 0:3], currInput[0, 0, 0, 3]), probabilities, currPlayer,
-                        currTree.root_node.state))
-            is_random = playTurnSparse(currTree.root_node.parent.game, currTree.root_node.state)
-            otherTree.move_opponent(currTree.root_node.game, currTree.root_node.state, is_random)
-            currTree.root_node.parent = None
-            otherTree.root_node.parent = None
+                         action))
+            # to wywolanie bedzie wrapperowane w jakas funkcje playturn czy cos podobnego
+            is_random = playTurnSparse(game, action)
+
+            for x in montecarlo:
+                x.sync_tree(game, action, is_random)
+                currTree.root_node.parent = None
+
             # if len(game.moves) >= 120:  # game too long, auto-draw
             #     break
     except GameOver as e:
-        if currTree.root_node.game.player1.playstate is PlayState.WON:
+        if game.player1.playstate is PlayState.WON:
             winner = 1
-        elif currTree.root_node.game.player2.playstate is PlayState.WON:
+        elif game.player2.playstate is PlayState.WON:
             winner = 2
         else:
             winner = 3
@@ -92,36 +99,33 @@ def playGame(model, simulations, seedObject = None):
         print(traceback.format_exc())
     return winner, data
 
-def child_finder(node, montecarlo):
 
-    if node.finished:
-        win_value = 1
-    else:
-        x = InputBuilder.convToInput(node.game, node.player_number)
-        expert_policy_values, win_value = montecarlo.model(x)
-        for action in checkValidActionsSparse(node.game):
-            child = Node(deepcopy(node.game))
-            child.state = action
-            is_random = 0
-            try:
-                is_random = playTurnSparse(child.game, action)
-            except GameOver:
-                child.finished = True
-            child.player_number = child.game.current_player.entity_id - 1
-            child.policy_value = expert_policy_values[0, action]
-            if is_random:
-                child.policy_value /= RANDOM_MOVE_SAMPLES
-            node.add_child(child)
-            if is_random:
-                for i in range(RANDOM_MOVE_SAMPLES - 1):
-                    child = Node(deepcopy(node.game))
-                    child.state = action
-                    try:
-                        playTurnSparse(child.game, action)
-                    except GameOver:
-                        child.finished = True
-                    child.player_number = child.game.current_player.entity_id - 1
-                    child.policy_value = expert_policy_values[0, action] / RANDOM_MOVE_SAMPLES
-                    node.add_child(child)
+def child_finder(node, montecarlo):
+    x = InputBuilder.convToInput(node.game, node.player_number)
+    expert_policy_values, win_value = montecarlo.model(x)
+    for action in checkValidActionsSparse(node.game):
+        child = Node(deepcopy(node.game))
+        child.state = action
+        is_random = 0
+        try:
+            is_random = playTurnSparse(child.game, action)
+        except GameOver:
+            child.finished = True
+        child.player_number = child.game.current_player.entity_id - 1
+        child.policy_value = expert_policy_values[0, action]
+        if is_random:
+            child.policy_value /= RANDOM_MOVE_SAMPLES
+        node.add_child(child)
+        if is_random:
+            for i in range(RANDOM_MOVE_SAMPLES - 1):
+                child = Node(deepcopy(node.game))
+                child.state = action
+                try:
+                    playTurnSparse(child.game, action)
+                except GameOver:
+                    child.finished = True
+                child.player_number = child.game.current_player.entity_id - 1
+                child.policy_value = expert_policy_values[0, action] / RANDOM_MOVE_SAMPLES
+                node.add_child(child)
     if node.parent is not None:
         node.update_win_value(float(win_value))
