524d08675fcb6cc38e28c4501fbbd10f5a0dd461
MSI\mifas <michal.zielonka77@gmail.com>
2022-09-12 22:34:47 +0200

    datarator fixes

--- DataGenerator.py
+++ DataGenerator.py
@@ -10,14 +10,30 @@ GAME_INDEX = 0
 
 class DataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
-    def __init__(self, list_IDs, batch_size=32, dim=(401, 41, 3), n_channels=1, shuffle=True):
+    def __init__(self,model_name, batch_size=32, dim=(401, 41, 3), n_channels=1, shuffle=True):
         'Initialization'
+        self.model_name = model_name
         self.dim = dim
         self.batch_size = batch_size
-        self.list_IDs = list_IDs
+        self.list_IDs = [*range(1,self.getNoTurns()+1)]
         self.n_channels = n_channels
         self.shuffle = shuffle
         self.on_epoch_end()
+
+    def getNoTurns(self):
+        noTurns = 0
+        for filepath in glob.iglob(f'../data/{self.model_name}/*'):
+            with open(filepath,"rb") as rb:
+                #METADATA AND DECK TO BE IGNORED
+                try:
+                    #SKIP METADATA
+                    pickle.load(rb)
+                    while True:
+                        game = pickle.load(rb)
+                        noTurns += len(game[0])
+                except EOFError:
+                    break
+        return noTurns
 
     def __len__(self):
         'Denotes the number of batches per epoch'
@@ -42,129 +58,88 @@ class DataGenerator(keras.utils.Sequence):
         if self.shuffle == True:
             np.random.shuffle(self.indexes)
 
-    def getGamesWithoutExceptionsFromSeveralFiles(numberOfGames: int):
-        cleanGames = []
-        global FILE_POSITION
-        FILE_POSITION = 0
-        for filepath in glob.iglob('data/Model-INIT/*'):
-            gamesInFile = DataGenerator.getGamesWithoutExceptions(filepath,num_games=numberOfGames)
-            if gamesInFile[0] == [] or gamesInFile[1] == []:
-                continue
-            cleanGames.append(gamesInFile)
-        return cleanGames
-
-    def getGamesWithoutExceptions(filepath:str, num_games:int =50):
-        global FILE_POSITION
-        arrOfGames = []
-        first_iteration = True
-
-        with open(filepath,"rb") as rb:
-            while FILE_POSITION < num_games:
-                try:
-                    if first_iteration:
-                        pickle.load(rb)
-                        first_iteration = not first_iteration
-                    elif FILE_POSITION <= num_games:
-                        tmp = pickle.load(rb)
-                        if not tmp:
-                            FILE_POSITION -= 1
-                            return
-                        if tmp[1] < 4: # mniejsze od 4 to dozwolone stany gry ( nie wyjątki )
-                            FILE_POSITION += 1
-                            if FILE_POSITION >= num_games:
-                                return arrOfGames
-                            arrOfGames.append(tmp)
-                        else:
-                            FILE_POSITION -= 1
-                except EOFError:
-                    return arrOfGames
-
-            return arrOfGames
-
-    def filter_list(L):
-        return
-
     # [ 5, 10 , 15, 20 ]
     # Zakladam ze lista ID bedzie miala najmniejszy mozliwy element == 1, zeby nie sprawdzac pierwszej tury gdzie jest deck. W razie co mozna po prostu dodac 1 do indeksu i po problemie :D
-    def getTurnsByIds(xyz = 0):#,listIds:[]):
-        listIds = [1,3,5,120]
-        idsToRemove = []
-        maxID = max(listIds)
-        turns = []
-        next = False
-        for filepath in glob.iglob('data/Model-INIT/*'):
-            breakCondition = True
+    def __data_generation(self,listIdsTmp:[]):#,listIds:[]):
+        listIds = sort(listIdsTmp)
+        X = []
+        yValue = []
+        yPolicy = []
+        for filepath in glob.iglob(f'../data/{self.model_name}/*'):
             with open(filepath,"rb") as rb:
-                #METADATA AND DECK TO BE IGNORED
-                metadata = pickle.load(rb)
-                while True:
-                    try:
-                        for idRemove in idsToRemove:
-                            if idRemove in listIds:
-                                listIds.remove(idRemove)
-                        if next:
-                            listIds = [id - len(game[0]) + 1 for id in listIds]
-                            next = False
-                        game = pickle.load(rb)
-                        for id in listIds:
+                try:
+                    #SKIP METADATA
+                    pickle.load(rb)
+                    while True:
+
+                            idsToRemove = []
+                            game = pickle.load(rb)
                             winner = game[1]
-                            if id <= len(game[0]) - 1:
-                                if game[0][id][2] == winner:
-                                    value = 1
-                                elif game[0][id][2] < 3:
-                                    value = -1
+
+                            for i,id in enumerate(listIds):
+                                if id <= len(game[0]) - 1:
+                                    if game[0][id][2] == winner:
+                                        value = 1
+                                    elif game[0][id][2] < 3:
+                                        value = -1
+                                    else:
+                                        value = 0
+                                    X.append(game[0][id][0])
+                                    yPolicy.append(game[0][id][1])
+                                    yValue.append(value)
+                                    idsToRemove.append(i)
                                 else:
-                                    value = 0
-                                turns.append((game[0][id][0],(game[0][id][1],value)))
-                                idsToRemove.append(id)
-                            else:
-                                next = True
-                                break
-                    except EOFError:
-                        break
+                                    break
 
-        return turns
+                            listIds = np.delete(listIds,idsToRemove)
 
+                            if listIds.size == 0:
+                                return np.asarray(X).squeeze(1), [np.asarray(yPolicy),np.asarray(yValue)]
 
-    def getNumberOfTurnsForOneGame(self):
+                            listIds = [id - len(game[0]) + 1 for id in listIds]
 
+                except EOFError:
+                    break
 
-        pass
-
-    def __data_generation(self, list_IDs_temp):
-        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
-        # Initialization
+        return np.asarray(X).squeeze(1), [np.asarray(yPolicy),np.asarray(yValue)]
 
 
-        # X = np.empty((self.batch_size, *self.dim, self.n_channels))
-        # y = np.empty((self.batch_size), dtype=int)
+    # def getGamesWithoutExceptionsFromSeveralFiles(numberOfGames: int):
+    #     cleanGames = []
+    #     global FILE_POSITION
+    #     FILE_POSITION = 0
+    #     for filepath in glob.iglob('data/Model-INIT/*'):
+    #         gamesInFile = DataGenerator.getGamesWithoutExceptions(filepath,num_games=numberOfGames)
+    #         if gamesInFile[0] == [] or gamesInFile[1] == []:
+    #             continue
+    #         cleanGames.append(gamesInFile)
+    #     return cleanGames
 
-
-        # for turn in game[0][1:]: # list of turns
-        #     if type(turn) is not str:
-        #         if winner < 3:
-        #             input = turn[0]
-        #             x.append(input)
-        #             policy = turn[1]
-        #             yPolicy.append(policy)
-        #             if turn[2] == winner:
-        #                 value = 1
-        #             elif turn[2] < 3:
-        #                 value = -1
-        #             else:
-        #                 value = 0
-        #             yValue.append(value)
-
-
-        # Generate data
-        for i, ID in enumerate(list_IDs_temp): # potrzebujemy wybierać konkretne gry z danego pliku zamiast pliku
-            # Store sample
-            minID = min(list_IDs_temp)
-            maxID = max(list_IDs_temp)
-            # X[i,] = np.load('data/' + ID + '.txt')
-            #
-            # # Store class
-            # y[i] = self.labels[ID]
-
-        return X, y
+    # def getGamesWithoutExceptions(filepath:str, num_games:int =50):
+    #     global FILE_POSITION
+    #     arrOfGames = []
+    #     first_iteration = True
+    #
+    #     with open(filepath,"rb") as rb:
+    #         while FILE_POSITION < num_games:
+    #             try:
+    #                 if first_iteration:
+    #                     pickle.load(rb)
+    #                     first_iteration = not first_iteration
+    #                 elif FILE_POSITION <= num_games:
+    #                     tmp = pickle.load(rb)
+    #                     if not tmp:
+    #                         FILE_POSITION -= 1
+    #                         return
+    #                     if tmp[1] < 4: # mniejsze od 4 to dozwolone stany gry ( nie wyjątki )
+    #                         FILE_POSITION += 1
+    #                         if FILE_POSITION >= num_games:
+    #                             return arrOfGames
+    #                         arrOfGames.append(tmp)
+    #                     else:
+    #                         FILE_POSITION -= 1
+    #             except EOFError:
+    #                 return arrOfGames
+    #
+    #         return arrOfGames
 
--- training/training.py
+++ training/training.py
@@ -4,6 +4,9 @@ import numpy as np
 import os
 import tensorflow as tf
 from tensorflow.keras import backend as k
+
+from HearthstoneRLBot.DataGenerator import DataGenerator
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
 MODEL_NAME = "Model-INIT"
@@ -17,41 +20,8 @@ config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
 
 def trainNetwork(model, trainingData):
-    trainingData = parseGames(trainingData)
-    data = []
-    for i in range(3):
-        with open(f"../data/{MODEL_NAME}/05-09-2022T164550.txt", "rb") as rb:
-            metadata = pickle.load(rb)
-            while True:
-                try:
-                    data.append(pickle.load(rb))
-                except (EOFError, pickle.UnpicklingError):
-                    break
-    x = []
-    yPolicy = []
-    yValue = []
-    for game in data:
-        winner = game[1] # who won the game
-        for turn in game[0][1:]: # list of turns
-            if type(turn) is not str:
-                if winner < 3:
-                    input = turn[0]
-                    x.append(input)
-                    policy = turn[1]
-                    yPolicy.append(policy)
-                    if turn[2] == winner:
-                        value = 1
-                    elif turn[2] < 3:
-                        value = -1
-                    else:
-                        value = 0
-                    yValue.append(value)
-    x = np.asarray(x)
-    yPolicy = np.asarray(yPolicy)
-    yValue = np.asarray(yValue)
-    x = x.squeeze(1)
-    model.fit(x=x, y=[yPolicy, yValue], batch_size=1, epochs=5, verbose=1)
-
+    trainingGenerator = DataGenerator(model_name='Model-INIT',batch_size=4)
+    model.fit_generator(generator=trainingGenerator,workers=1,use_multiprocessing=False,epochs=5)
     model.save('../Model/models/TRAINED_MODEL')
 
 def parseGames(games):
