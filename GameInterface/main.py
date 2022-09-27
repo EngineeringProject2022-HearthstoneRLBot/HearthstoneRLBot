eaec886cbb3624c375bf42e84d821f0122a8d428
KacperPietraszek <kacper.pietraszek@autonomik.pl>
2022-09-27 17:51:20 +0200

    GameCreator - one to rule them all, function in it can start most of games that we need, ex model player vs random player, main - short user manual of GameCreator

--- GameInterface/main.py
+++ GameInterface/main.py
@@ -3,25 +3,25 @@ from GameInterface.AIPlayer import AIPlayer
 from GameInterface.ManualPlayer import ManualPlayer
 from GameInterface.ModeledGame import ModeledGame
 from GameInterface.RandomPlayer import RandomPlayer
+from GameCreator import GameCreator
+from GameCreator import HeroDecks
+from GameCreator import PlayerType
 
+
+# GameCreator.createDefaultGame() user Manual:
+# Arguments:
+# player1/2Type --> type of player (ex.Random/Modeled) if you pass only player1Type then player2Type will be the same
+# modelp1/p2    --> model that player will use,        if you pass only modelp1 then the players will use the same model
+# depthp1/p2    --> number of simulations in the tree, if you pass only depthp1 then the players will have the same depth
+# p1/p2         --> basically decks for players that we can give manually, use HeroDecks. ... and just choose the deck USE "()" p1=()
+# createDefaultGame(playerType=PlayerType.Random)  - Basic random vs random moves on randomly picked decks
+# createDefaultGame(playerType=PlayerType.Modeled) - Basic model  vs model  moves on randomly picked decks
+# createDefaultGame(player1Type=PlayerType.Modeled, modelp1="NAZWA_MODELU", depthp1=25) #both players on same model->NAZWA_MODELU and on the same depth=25
+# createDefaultGame(player1Type=PlayerType.Modeled, modelp1="NAZWA_MODELU", depthp1=25 ,p1=(HeroDecks.BasicDruid), player2Type=PlayerType.Random, p2=(HeroDecks.FastDruid), printLogs=True)
 
 def main():
-    print('x')
     cards.db.initialize()
-    deck1 = ['EX1_169', 'EX1_169', 'EX1_154', 'EX1_154', 'CS2_011', 'CS2_011', 'CS2_013', 'CS2_013', 'EX1_166',
-             'EX1_166', 'CS2_012', 'CS2_012', 'EX1_165', 'EX1_165', 'EX1_571', 'NEW1_008', 'NEW1_008', 'EX1_573',
-             'FP1_001', 'EX1_085', 'FP1_005', 'FP1_005', 'EX1_005', 'GVG_096', 'GVG_096', 'EX1_284', 'EX1_558',
-             'FP1_012', 'EX1_016', 'GVG_110']
-    deck2 = ['GVG_043', 'GVG_043', 'BRM_013', 'BRM_013', 'NEW1_031', 'NEW1_031', 'EX1_536', 'EX1_536', 'EX1_539',
-             'EX1_539', 'EX1_538', 'EX1_538', 'CS2_188', 'CS2_188', 'EX1_008', 'EX1_008', 'EX1_029', 'EX1_029',
-             'FP1_002', 'FP1_002', 'NEW1_019', 'NEW1_019', 'AT_084', 'AT_084', 'EX1_089', 'EX1_089', 'AT_087', 'AT_087',
-             'CS2_203', 'CS2_203']
-    p1 = AIPlayer("X", 3, deck1, "Model-INIT", 2)
-    #p2 = AIPlayer("Y", 4, deck2, "Model-INIT", 2)
-    #p1 = RandomPlayer("X", 3, deck1)
-    #p2 = RandomPlayer("Y", 4, deck2, False)
-    p2 = ManualPlayer("Ja", 5, deck2)
-    game = ModeledGame(p1, p2)
+    game = GameCreator.createDefaultGame(player1Type=PlayerType.Modeled, modelp1="XYZ") #both players on the same model: XYZ
     game.start()
 
 if __name__ == "__main__":
