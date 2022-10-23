ba200b4633a67b86ab3010e101a0ec35db8d776a
Kacper <kacperkrzeminski123@gmail.com>
2022-10-23 20:51:34 +0200

    custom loss func

--- training/training.py
+++ training/training.py
@@ -34,9 +34,55 @@ config = ConfigProto()
 config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
 
-model = tf.keras.models.load_model(f"../Model/models/{Configuration.INPUT_MODEL_NAME}")
-model.compile(loss=['categorical_crossentropy', 'mean_squared_error'],
-              loss_weights = [Configuration.POLICY_WEIGHT, Configuration.WINVALUE_WEIGHT], optimizer=tf.keras.optimizers.Adam(Configuration.LEARNING_RATE))
+
+def weighted_categorical_crossentropy(weights):
+    """
+    A weighted version of keras.objectives.categorical_crossentropy
+
+    Variables:
+        weights: numpy array of shape (C,) where C is the number of classes
+
+    Usage:
+        weights = np.array([0.5,2,10]) # Class one at 0.5, class 2 twice the normal weights, class 3 10x.
+        loss = weighted_categorical_crossentropy(weights)
+        model.compile(loss=loss,optimizer='adam')
+    """
+
+    weights_heavy = weights[:]
+    weights_heavy[-1] = 10
+    _weights = K.variable(weights)
+    _weights_heavy = K.variable(weights_heavy)
+
+    def loss(y_true, y_pred):
+        # scale predictions so that the class probas of each sample sum to 1
+        y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
+        # clip to prevent NaN's and Inf's
+        y_pred = K.clip(y_pred, K.epsilon(), 1 - K.epsilon())
+        # calc
+        check = K.greater(y_pred[0, -1], y_true[0, -1])
+        weights_final = K.switch(
+            check,
+            _weights_heavy,
+            _weights
+        )
+        loss = y_true * K.log(y_pred) * weights_final
+        loss = -K.sum(loss, -1)
+        return loss
+
+    return loss
+
+
+loss = weighted_categorical_crossentropy([np.ones(252)])
+model = tf.keras.models.load_model(f"../Model/models/{Configuration.INPUT_MODEL_NAME}", custom_objects={"loss": loss})
+
+boundaries = [10000, 80000]
+values = [0.00001, 0.00001, 0.000001]
+lr = tf.keras.optimizers.schedules.PiecewiseConstantDecay(boundaries, values)
+# lr = Configuration.LEARNING_RATE
+
+model.compile(loss=[loss, 'mean_squared_error'],
+              loss_weights=[Configuration.POLICY_WEIGHT, Configuration.WINVALUE_WEIGHT],
+              optimizer=tf.keras.optimizers.Adam(lr))
 # model.optimizer = tf.keras.optimizers.Adam(
 #     learning_rate=0.001,
 #     beta_1=0.9,
